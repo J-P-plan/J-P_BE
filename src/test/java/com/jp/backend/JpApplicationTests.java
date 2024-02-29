7ad439ed6ff1383cf297c177b0526021adf7babe
Chaeeunm <rlacodms0525@naver.com>
2024-02-29 20:14:42 +0900

    initial setting

---
+++ src/test/java/com/jp/backend/JpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
