92b202e5890bc060633ea30e61a50603cd4dfea5
Chaeeunm <rlacodms0525@naver.com>
2024-03-14 23:28:28 +0900

    Fix : NoArgs 생성자 추가

--- src/main/java/com/jp/backend/domain/user/dto/UserPostDto.java
+++ src/main/java/com/jp/backend/domain/user/dto/UserPostDto.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -17,6 +18,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @Builder
+@NoArgsConstructor
 public class UserPostDto {
 	@NotNull
 	@Email
