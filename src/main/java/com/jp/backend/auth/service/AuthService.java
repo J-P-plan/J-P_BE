fce3cd9450676d4643b866698ee35cc50cafdadf
Joo614 <57wnguswn57@gmail.com>
2024-03-26 01:11:07 +0900

    Refactor: TODO 및 주석 삭제

--- src/main/java/com/jp/backend/auth/service/AuthService.java
+++ src/main/java/com/jp/backend/auth/service/AuthService.java
@@ -2,14 +2,11 @@ package com.jp.backend.auth.service;
 
 import static com.jp.backend.auth.utils.HeaderUtils.*;
 
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Service;
 
-import com.jp.backend.auth.config.JwtConfig;
 import com.jp.backend.auth.repository.RefreshTokenRepository;
 import com.jp.backend.auth.token.AuthToken;
 import com.jp.backend.auth.token.AuthTokenProvider;
-import com.jp.backend.domain.user.repository.JpaUserRepository;
 import com.jp.backend.global.exception.CustomLogicException;
 import com.jp.backend.global.exception.ExceptionCode;
 
@@ -24,12 +21,6 @@ import lombok.RequiredArgsConstructor;
 public class AuthService {
 	private final RefreshTokenRepository refreshTokenRepository;
 	private final AuthTokenProvider authTokenProvider;
-	private final JpaUserRepository userRepository;
-	private final AuthenticationManager authenticationManager;
-	private final JwtConfig jwtConfig;
-	private final static String HEADER_AUTHORIZATION = "Authorization";
-	private final static String TOKEN_PREFIX = "Bearer";
-	// TODO : 여기 필요없는 DI 삭제
 	// TODO : 로그아웃 Redis 추가?
 
 	public void logout(HttpServletRequest request, HttpServletResponse response) {
