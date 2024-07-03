9fd2f799c05b9a2fd24eeacdae54cecb16ec9033
chaeeunm <rlacodms70@gmail.com>
2024-07-04 08:52:19 +0900

    Feat: 충남 도시 추가, ID 겹치지 않게 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,95 +1,83 @@
--- place - CITY
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (1, 'ChIJ1a3vsrjjZTURMC44oCngkro', '대구', '대구광역시 대구',
-        '대한민국의 대표적인 내륙 도시입니다. 대구는 대한민국의 동남부에 위치한 광역시로, 경제, 문화, 교육, 의료 등 다양한 분야에서 중요한 역할을 하고 있습니다. 대구는 또한 섬유 산업의 중심지로 유명하며, 매년 섬유 패션 전시회가 개최되어 많은 관광객과 바이어들이 방문합니다.',
-        'CITY', 35.8294374, 128.5655119);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (2, 'ChIJY9gWA0MjcDURmkLDkeAXvMc', '전주', '전라북도 전주', '한국 전통문화의 중심지로 잘 알려진 도시입니다.', 'CITY', 35.8280463,
-        127.1160156);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (3, 'ChIJRUDITFTjDDURMb8emNI2vGY', '제주도', '제주특별자치도 제주도', '대한민국 최남단에 위치한 아름다운 섬입니다.', 'CITY', 33.3846216,
-        126.5534925);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (4, 'ChIJNc0j6G3raDURpwhxJHTL2DU', '부산', '부산광역시 부산', '대한민국의 제2의 도시이자, 주요 항구 도시입니다.', 'CITY', 35.2100142,
-        129.0688702);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (5, 'ChIJJwWh07tVbzURgw9_QGytjDU', '창원', '경상남도 창원', '경상남도의 중심 도시로, 산업과 자연이 조화를 이루는 곳입니다.', 'CITY', 35.2028593,
-        128.6000923);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (6, 'ChIJWw9PleHlYTURRh09nFHGt4A', '강릉', '강원도 강릉', '동해안에 위치한 아름다운 해변과 커피 거리로 유명한 도시입니다.', 'CITY', 37.7091295,
-        128.8324462);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (7, 'ChIJR4ITliVveTURQmG3LJD9N30', '인천', '인천광역시 인천', '대한민국의 서해안에 위치한 주요 항구 도시입니다.', 'CITY', 37.4562557,
-        126.7052062);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (8, 'ChIJr6f1ASOJcTURSPUlAe3S9AU', '광주', '광주광역시 광주', '대한민국의 남서부에 위치한 문화와 예술의 도시입니다.', 'CITY', 35.1557358,
-        126.8354271);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (9, 'ChIJAWZKutdIZTURtdOKmJ3WltE', '대전', '대전광역시 대전', '대한민국 중부에 위치한 과학기술의 중심 도시입니다.', 'CITY', 36.3398175,
-        127.3940486);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (10, 'ChIJgd6y4osuZjURATHZM3P6g3A', '울산', '울산광역시 울산', '대한민국의 동남부에 위치한 산업 도시입니다.', 'CITY', 35.5537228,
-        129.2380554);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (11, 'ChIJ0x17PwR1YzURpfes2eU-Vl4', '원주', '강원도 원주', '강원도의 중심 도시로, 다양한 문화와 자연이 어우러진 도시입니다.', 'CITY', 37.3082307,
-        127.9294889);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (12, 'ChIJEUZ2IApDezURybRd7gIwN_E', '수원', '경기도 수원', '경기도의 중심 도시로, 화성이 유명한 역사적인 도시입니다.', 'CITY', 37.2803896,
-        127.0077847);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (13, 'ChIJfa9k5K0nZTURiYhqfJx4mfw', '청주', '충청북도 청주', '충청북도의 중심 도시로, 풍부한 역사와 함께 현대적인 면모를 갖춘 도시입니다.', 'CITY',
-        36.6272962, 127.498731);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (14, 'ChIJTUI5jYAUcDURPIpSuFk4EOs', '익산', '전라북도 익산', '전라북도에 위치한 역사적이며 문화적인 가치가 높은 도시입니다.', 'CITY', 36.0230799,
-        126.9894962);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (15, 'ChIJX7zzpqekczURlIbIAK4ltrs', '목포', '전라남도 목포', '전라남도에 위치한 해안 도시로, 뛰어난 해산물과 아름다운 해변이 특징인 도시입니다.', 'CITY',
-        34.8118351, 126.3921664);
 
--- place - TRAVEL_PLACE
+--충북
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (16, 'ChIJR7Y1TBFyYzURB9CrhSkMGuE', '소금산 출렁다리', '강원도 원주', '강원도 원주에 위치한 아름다운 출렁다리입니다.', 'TRAVEL_PLACE',
-        37.3676264, 127.8248756);
+VALUES (501, 'ChIJfa9k5K0nZTURiYhqfJx4mfw', '청주', '충청북도 청주', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.6272962, 127.498731);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (17, 'ChIJT8R-JJA1fTURu_F8WZJr7OU', '한탄강 주상절리길', '강원도 철원', '한탄강을 따라 이어지는 신비로운 주상절리길입니다.', 'TRAVEL_PLACE',
-        38.1595893, 127.2859219);
+VALUES (502, 'ChIJgRGbRi6EZDURpIsGqHmxkoY', '충주', '충청북도 충주', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 37.0151461, 127.8956693);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (18, 'ChIJVRLdCsjtaDURcdummQTLnRk', '오륙도 스카이워크', '부산광역시 남구', '부산 남구에 위치한 아름다운 해안 절경을 자랑하는 오륙도입니다.',
-        'TRAVEL_PLACE', 35.07805, 129.08751);
+VALUES (503, 'ChIJoxTVVcGHYzUR1BvZ2nCGjc0', '제천', '충청북도 제천', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 37.0597328, 128.141005);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (19, 'ChIJW-FwpwdDDDURKKvdKdJhhF8', '용머리 해안', '제주특별자치도 서귀포', '제주 서귀포에 위치한 독특한 지형의 해안가입니다.', 'TRAVEL_PLACE',
-        33.2316155, 126.3148321);
+VALUES (504, 'ChIJVwAKd0IZZTURmlMBLcnKkxQ', '보은', '충청북도 보은', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.4899504, 127.729342);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (20, 'ChIJc6ey6CDZbTURY6R4148iqfM', '여수 해상 케이블카', '전라남도 여수', '여수의 아름다운 해상 풍경을 감상할 수 있는 케이블카입니다.', 'TRAVEL_PLACE',
-        34.73056, 127.741572);
+VALUES (505, 'ChIJhaqof9wUZTURW7xaGKUSM00', '옥천', '충청북도 옥천', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.3203938, 127.6566592);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (21, 'ChIJuVQp1w682F8RULr4nyAoHWU', '영금정', '강원도 속초', '속초의 아름다운 해안 절경을 감상할 수 있는 영금정입니다.', 'TRAVEL_PLACE',
-        38.2118554, 128.6014858);
+VALUES (506, 'ChIJPVedZBNuZTURvx2uR-6t1ZI', '영동', '충청북도 영동', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.1597002, 127.8142287);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (22, 'ChIJGzvMzrCYcTURd4DZLzgyFG8', '고창읍성', '전라북도 고창', '고창에 위치한 조선시대의 역사적인 읍성입니다.', 'TRAVEL_PLACE', 35.4313384,
-        126.7042361);
+VALUES (507, 'ChIJQSAi8A_PZDUR58l41c1uqyE', '진천', '충청북도 진천', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.8708978, 127.4405915);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (23, 'ChIJRYAZP6n_DDURGnc9lYwcZ38', '사라오름', '제주특별자치도 제주시', '제주도의 아름다운 오름 중 하나입니다.', 'TRAVEL_PLACE', 33.3709058,
-        126.5704745);
+VALUES (508, 'ChIJfVXxCFOUZDUR2d4WqpGzmvo', '음성', '충청북도 음성', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.9761637, 127.6142294);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (24, 'ChIJ9SUiOHBOZjUR_YnH8Lbjzt0', '경주 동궁과 월지', '경상북도 경주', '신라시대의 궁궐과 연못이 있는 역사적인 장소입니다.', 'TRAVEL_PLACE',
-        35.8348173, 129.2265645);
+VALUES (509, 'ChIJfTIx8JrtZDUR8l4zh7ba7wA', '괴산', '충청북도 괴산', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7697608, 127.8294253);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (25, 'ChIJod7tSseifDUR9hXHLFNGMIs', '경복궁', '서울특별시 종로', '조선 시대의 대표적인 궁궐로, 다양한 문화재와 함께 서울의 중심부에 위치합니다.',
-        'TRAVEL_PLACE', 37.579617, 126.977041);
+VALUES (510, 'ChIJdykXlzLxYzURV9_CbQ04nbI', '단양', '충청북도 단양', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.9941896, 128.3877555);
 INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (26, 'ChIJxw7HJy_taDUR-xaSTeHwbf8', '광안리해수욕장', '부산광역시 광안리해수욕장', '부산의 대표적인 해변 가운데 하나로, 밤에 보는 광안대교의 야경이 유명합니다.',
-        'TRAVEL_PLACE', 35.15396, 129.11848);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (27, 'ChIJn3jj9rkUDTURS7YjOgUyUVU', '제주도 성산일출봉', '제주특별자치도 제주도', '제주도의 대표적인 관광지로, 해안 절벽 위에 위치한 화산 분화구입니다.',
-        'TRAVEL_PLACE', 33.458056, 126.94250);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (28, 'ChIJQwyfbNqyZzUR31ffyfmvm6w', '경주 불국사', '경상북도 경주', '신라 시대의 불교 사원으로, 유네스코 세계문화유산에 등재되어 있습니다.',
-        'THEME', 35.7901973, 129.3320806);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (29, 'ChIJS73uEmIjcDURjoTQxhu-9I4', '전주 한옥마을', '전라북도 전주', '전통 한옥이 잘 보존된 지역으로, 한국의 전통 문화를 체험할 수 있는 곳입니다.',
-        'THEME', 35.8175376, 127.1520417);
-INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
-VALUES (30, 'ChIJda9gFeQmYzURIsXnKaOqStY', '남이섬', '강원도 춘천', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
-        'THEME', 37.7899352, 127.5258072);
+VALUES (511, 'ChIJPXkHCtrCZDURN9AADeggtz0', '증평', '강원도 춘천', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7864783, 127.6046068);
 
+--충남
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES VALUES (512, 'ChIJ6QUVEkr_ejURkBv31pQCCbc', '당진', '충청남도 당진', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+           'CITY', 36.9023743, 126.6545022);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (513, 'ChIJswpwSZtbejUR6U6X3CLP3hA', '서산', '충청남도 서산', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7844993, 126.4503169);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (514, 'ChIJZxmSSfASejURQE9m6F6Ov9w', '태안', '충청남도 태안', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY',  36.7456421, 126.2980528);
+
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (515, 'ChIJVwAKd0IZZTURmlMBLcnKkxQ', '홍성', '충청남도 홍성', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.4899504, 127.729342);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (516, 'ChIJhaqof9wUZTURW7xaGKUSM00', '청양', '충청남도 청양', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.3203938, 127.6566592);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (517, 'ChIJPVedZBNuZTURvx2uR-6t1ZI', '보령', '충청남도 보령', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.1597002, 127.8142287);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (518, 'ChIJQSAi8A_PZDUR58l41c1uqyE', '서천', '충청남도 서천', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.8708978, 127.4405915);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (519, 'ChIJfVXxCFOUZDUR2d4WqpGzmvo', '아산', '충청남도 아산', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.9761637, 127.6142294);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (520, 'ChIJfTIx8JrtZDUR8l4zh7ba7wA', '천안', '충청남도 천안', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7697608, 127.8294253);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (521, 'ChIJdykXlzLxYzURV9_CbQ04nbI', '예산', '충청남도 예산', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.9941896, 128.3877555);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (522, 'ChIJPXkHCtrCZDURN9AADeggtz0', '공주', '충청남도 공주', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7864783, 127.6046068);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (523, 'ChIJPXkHCtrCZDURN9AADeggtz0', '계룡', '충청남도 계룡', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7864783, 127.6046068);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (524, 'ChIJPXkHCtrCZDURN9AADeggtz0', '금산', '충청남도 금산', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7864783, 127.6046068);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (525, 'ChIJPXkHCtrCZDURN9AADeggtz0', '논산', '충청남도 논산', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7864783, 127.6046068);
+INSERT INTO place (ID, PLACE_ID, NAME, SUB_NAME, DESCRIPTION, PLACE_TYPE, LAT, LNG)
+VALUES (526, 'ChIJPXkHCtrCZDURN9AADeggtz0', '부여', '충청남도 부여', '남이섬은 춘천시에 위치한 소양강에 있는 작은 섬으로, 사계절 내내 다양한 자연경관을 즐길 수 있습니다.',
+        'CITY', 36.7864783, 127.6046068);
