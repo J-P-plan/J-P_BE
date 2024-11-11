ed6ef24f1082b6db93773eb77afa4a2ab8307589
Joo614 <57wnguswn57@gmail.com>
2024-11-11 15:40:07 +0900

    Refactor: config 수정

--- src/main/java/com/jp/backend/domain/file/config/S3BucketConfig.java
+++ src/main/java/com/jp/backend/domain/file/config/S3BucketConfig.java
@@ -2,6 +2,7 @@ package com.jp.backend.domain.file.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -9,6 +10,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
+@Configuration
 public class S3BucketConfig {
 	@Value("${cloud.aws.credentials.accessKey}")
 	private String accessKey;
