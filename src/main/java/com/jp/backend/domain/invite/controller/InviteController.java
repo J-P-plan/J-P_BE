ee66b00a0c888255b41e7149b7fb674960d7e025
Joo614 <57wnguswn57@gmail.com>
2025-01-09 11:49:12 +0900

    Comment: 주석 추가

--- src/main/java/com/jp/backend/domain/invite/controller/InviteController.java
+++ src/main/java/com/jp/backend/domain/invite/controller/InviteController.java
@@ -28,9 +28,14 @@ public class InviteController {
 	@PostMapping
 	public ResponseEntity invite(@Valid @RequestBody InviteReqDto reqDto,
 		@AuthenticationPrincipal UserPrincipal principal) {
+		// 서비스 코드에서는 RedisUtil을 주입받고 Redis에 저장된 teamId에 해당하는 값이 있는지 확인
+		// 만약 초대 코드가 이미 생성되어 redis에 해당하는 값이 있다면, 해당 코드를 반환
+		// 생성된 초대 코드가 없다면, 랜덤한 문자열을 만들어 value로 지정하고, 유효기간 1일의 TTL을 설정
+		// 초대 링크 만든 사람이면 그냥 들어가지게? 그 사람이 들어갔는데 초대한다고 뜨면 안되잖아
 
 		return ResponseEntity.ok().build();
 	}
+	// TODO 초대 코드 검증 ?
 
 	@Operation(summary = "수락 API")
 	@PostMapping("/{inviteId}/accept")
